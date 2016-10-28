130d979da786f05f577348683cd512945f3c9008
John Nash <redd@redd.ink>
2016-10-28 15:32:00 +1100

    update for Reddcoin

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='bitcoin',
+setup(name='pyreddcointools',
       version='1.1.42',
-      description='Python Bitcoin Tools',
-      author='Vitalik Buterin',
-      author_email='vbuterin@gmail.com',
-      url='http://github.com/vbuterin/pybitcointools',
-      packages=['bitcoin'],
+      description='Python Reddcoin Tools',
+      author='Vitalik Buterin, John Nash',
+      author_email='vbuterin@gmail.com, john@redd.ink',
+      url='http://github.com/reddink/pyreddcointools',
+      packages=['pyreddcointools'],
       scripts=['pybtctool'],
       include_package_data=True,
-      data_files=[("", ["LICENSE"]), ("bitcoin", ["bitcoin/english.txt"])],
+      data_files=[("", ["LICENSE"]), ("pyreddcointools", ["pyreddcointools/english.txt"])],
       )
