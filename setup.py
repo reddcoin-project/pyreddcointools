a8ad686182419ff9aaa57a07e208e837abd52dae
John Nash <redd@redd.ink>
2017-09-30 10:32:49 +1000

    update setup for pyreddcointools

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='pyreddcointools',
-      version='1.1.42',
+      version='1.1.43',
       description='Python Reddcoin Tools',
       author='Vitalik Buterin, John Nash',
       author_email='vbuterin@gmail.com, john@redd.ink',
-      url='http://github.com/reddink/pyreddcointools',
+      url='http://github.com/reddcoin-project/pyreddcointools',
       packages=['pyreddcointools'],
-      scripts=['pybtctool'],
+      scripts=['pyrddtool'],
       include_package_data=True,
       data_files=[("", ["LICENSE"]), ("pyreddcointools", ["pyreddcointools/english.txt"])],
       )
